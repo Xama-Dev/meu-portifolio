23da478237e72d2171cacce36f979260819b6a00
Tiago Moreira <bugnevermore@gmail.com>
2022-12-27 14:17:54 -0300

    criação do arquivo app.js, index.html e os modulos express e cors para configuração do servidor web.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+const { response } = require('express')
+
+app.use(cors())
+
+app.get('/', (request, response)=> {
+    response.sendFile('/home/tiagolinux/meuPortifolio/index.html')
+})
+
+app.listen(port, ()=> {
+    console.log(`Aplicação rodando na porta ${port}.`)
+})
