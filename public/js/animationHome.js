381d572eb4845620ab70db461a5e6aa2eaa55346
Tiago Moreira <bugnevermore@gmail.com>
2022-12-28 18:50:21 -0300

    Implementado: Banner com texto de saudação e animação no meu nome com underline piscando; Seção de contatos e minhas redes sociais com efeito de slide lateral e show fadeIn.

---
+++ public/js/animationHome.js
@@ -0,0 +1,10 @@
+let containerPresentationText = document.getElementById('presentation-text')
+let conteinerContatos = document.getElementById('contatiner-contatos')
+let containerRedesSociais = document.getElementById('container-redesSociais')
+
+containerPresentationText.classList.add('showContent')
+conteinerContatos.classList.add('showContent')
+containerRedesSociais.classList.add('showContent')
+
+conteinerContatos.setAttribute('style', 'margin-left: 20%')
+containerRedesSociais.setAttribute('style', 'margin-right: 20%') 
