7310895bb933b10c9a85642bce0f6a45e07e7b6a
Tiago Moreira <bugnevermore@gmail.com>
2022-12-29 18:21:05 -0300

    Implementado: Ativação do btn-link da seção no menu Nav conforme scroll da tela; Mudança do nome da seção Home para Presentation.

--- public/js/menuNav.js
+++ public/js/menuNav.js
@@ -1,21 +1,3 @@
-// let linksMenuNav = document.querySelectorAll('nav a')
-
-// linksMenuNav.forEach((link)=> {
-    
-//     link.classList.remove('btn-nav-active')
-
-//     // if(!link.classList.contains('btn-nav-active')) {
-
-//     // }
-//     link.addEventListener('click', ()=> {
-//         link.classList.add('btn-nav-active')
-//         // if(!link.classList.contains('btn-nav-active')) {
-//         // }
-//         // else {
-//         //     link.classList.remove('btn-nav-active')
-//         // }
-//     })
-// }) 
 
 function linkNavActive (link) {
     let linksMenuNav = document.querySelectorAll('nav a')
@@ -27,3 +9,57 @@ function linkNavActive (link) {
     link.classList.add('btn-nav-active')
 
 }
+
+
+document.addEventListener('scroll', (event)=> {
+    let linksMenuNav = document.querySelectorAll('nav a')
+    let screenHeightHalf = window.innerHeight / 2
+
+    let btnNavPresentation = document.getElementById('btn-nav-presentation')
+
+    let containerSkillsPosition = document.getElementById('container-skills').getBoundingClientRect().y
+    let btnNavSkills = document.getElementById('btn-nav-skills')
+
+    let containerAdvantagesPosition = document.getElementById('container-advantages').getBoundingClientRect().y
+    let btnNavAdvantages = document.getElementById('btn-nav-advantages')
+
+    let containerPortfolioPosition = document.getElementById('container-portfolio').getBoundingClientRect().y
+    let btnNavPortfolio = document.getElementById('btn-nav-portfolio')
+
+    if(containerSkillsPosition < screenHeightHalf) {
+        
+        linksMenuNav.forEach((link)=> {
+            link.classList.remove('btn-nav-active')
+        })
+
+        btnNavSkills.classList.add('btn-nav-active')
+    }
+
+    if(containerSkillsPosition > screenHeightHalf) {
+        
+        linksMenuNav.forEach((link)=> {
+            link.classList.remove('btn-nav-active')
+        })
+
+        btnNavPresentation.classList.add('btn-nav-active')
+    }
+
+    if(containerAdvantagesPosition < screenHeightHalf) {
+        
+        linksMenuNav.forEach((link)=> {
+            link.classList.remove('btn-nav-active')
+        })
+
+        btnNavAdvantages.classList.add('btn-nav-active')
+    }
+
+    if(containerPortfolioPosition < screenHeightHalf) {
+        
+        linksMenuNav.forEach((link)=> {
+            link.classList.remove('btn-nav-active')
+        })
+
+        btnNavPortfolio.classList.add('btn-nav-active')
+    }
+
+})
