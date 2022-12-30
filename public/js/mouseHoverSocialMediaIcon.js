d7edce1fda95917e9a8f35c06337a28d2d149ba6
Tiago Moreira <bugnevermore@gmail.com>
2022-12-30 11:49:18 -0300

    Implementado: Api WhatsApp no click do btn. Efeito inflate no mouseover.

--- public/js/mouseHoverSocialMediaIcon.js
+++ public/js/mouseHoverSocialMediaIcon.js
@@ -1,16 +1,30 @@
 let linksSocialMedia = document.querySelectorAll('.container-redesSociais a')
 
+let linkWhatsApp = document.getElementById('link-whatsApp')
+let iconWhatsApp = document.getElementById('icon-whatsApp')
+let tagWhatsApp = linkWhatsApp.childNodes[3]
+
+
 linksSocialMedia.forEach((link)=> {
     let icon = link.firstElementChild
     let nameSocialMedia = link.childNodes[3]
     
     link.addEventListener('mouseover', () => {
-        icon.classList.add('icon-socialMedia-inflate')
+        icon.classList.add('icon-inflate')
         nameSocialMedia.setAttribute('style', 'font-size: 14px')
     })
     link.addEventListener('mouseout', () => {
-        icon.classList.remove('icon-socialMedia-inflate')
+        icon.classList.remove('icon-inflate')
         nameSocialMedia.setAttribute('style', 'font-size: 17px')
         // nameSocialMedia.style.fontSize = '17px'
     })
+})
+
+linkWhatsApp.addEventListener('mouseover', () => {
+    iconWhatsApp.classList.add('icon-inflate')
+    tagWhatsApp.setAttribute('style', 'font-size: 14px')
+})
+linkWhatsApp.addEventListener('mouseout', () => {
+    iconWhatsApp.classList.remove('icon-inflate')
+    tagWhatsApp.setAttribute('style', 'font-size: 17px')
 })
