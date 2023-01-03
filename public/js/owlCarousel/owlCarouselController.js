bf86b0930851e305e4bb52c2fbdb52f06e5a7a93
Tiago Moreira <bugnevermore@gmail.com>
2023-01-03 20:46:12 -0300

    Seção portifolio: Implementado o layout com carousel.

--- public/js/owlCarousel/owlCarouselController.js
+++ public/js/owlCarousel/owlCarouselController.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel-advantages").owlCarousel({
+        loop:false,
+        margin:50,
+        responsiveClass:false,
+        nav: true,
+        items: 1,
+        center: false,
+        autoplay: false,
+        autoplayHoverPause: true
+
+        
+    });
+});
+
+$(document).ready(function() {
+    $(".owl-carousel-portfolio").owlCarousel({
         loop:false,
         margin:50,
         responsiveClass:false,
