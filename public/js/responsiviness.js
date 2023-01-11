aa44f94562128f6ce0748755ede28c6120b36881
Tiago Moreira <bugnevermore@gmail.com>
2023-01-11 18:44:13 -0300

    Responsividade da seção portfolio: Refatorado o código de responsividade da seção para mais clareza na leitura e melhor layout em diferentes tamanhos de tela.

--- public/js/responsiviness.js
+++ public/js/responsiviness.js
@@ -1,5 +1,7 @@
-//window.addEventListener('resize',
-window.onresize = () => {
+window.addEventListener('resize', responsiveCarouselPortfolio);
+window.addEventListener('load', responsiveCarouselPortfolio)
+
+ function responsiveCarouselPortfolio () {
 	let screenWidth = window.innerWidth
     let navCarouselPortfolio = document.getElementsByClassName('owl-nav')[1]
     let nextButton = document.getElementsByClassName('owl-next')[1]
@@ -11,39 +13,81 @@ window.onresize = () => {
     let infoProjectCollection = document.querySelectorAll('.info-project') 
 
 
-    if (screenWidth < 1320) {
-        navCarouselPortfolio.classList.add('display-none')
-        containerCarouselPortfolio.classList.add('width-100Percent')        
-    
-    }
-    if(screenWidth >= 1320) {
+    if(screenWidth > 1090) {        
+        
+        //----Undo < 940px && 1320px >  
         navCarouselPortfolio.classList.remove('display-none')
-        containerCarouselPortfolio.classList.remove('width-100Percent')
-    }
-    if (screenWidth < 1225) {
-        videoCarouselPortfolio.forEach((video) => {
-            video.style.width = '570px'
-            
-        })
-
-        containerProject.forEach((containerProject) => {
-            containerProject.style.padding = '0px'
-        })
-
-        containerCarouselPortfolio.style.paddingTop = '100px'
-    }
-    if (screenWidth >= 1225) {
-        videoCarouselPortfolio.forEach((video) => {
-            video.style.width = '700px'
-        })
-
+        
         containerProject.forEach((containerProject) => {
             containerProject.style.padding = '35px 85px'
         })
+        //--------------------------------
 
-        containerCarouselPortfolio.style.paddingTop = '50px'
+
+        //----Undo < 940px
+        dotsCarouselPortfolio.classList.remove('display-none')
+        navCarouselPortfolio.classList.add('owl-nav-portfolio-style')
+        navCarouselPortfolio.classList.remove('nav-style-screen940px')
+        nextButton.classList.add('owl-next-carouselPortfolio-style')
+        prevButton.classList.add('owl-prev-carouselPortfolio-style')
+        nextButton.classList.remove('buttonsNav-portfolio-screen940px')
+        prevButton.classList.remove('buttonsNav-portfolio-screen940px')
+
+        containerProject.forEach((containerProject) => {
+            containerProject.classList.remove('responsive-displayColumn-carouselPortfolio')
+        })
+
+        videoCarouselPortfolio.forEach((video) => {
+            video.style.width = '60%'
+            video.style.marginBottom = '0px'
+        })
+
+        infoProjectCollection.forEach((infoProject) => {
+            infoProject.classList.remove('infoProject-style-screen940px')
+        })
+        //-----------------------------------------------
     }
-    if (screenWidth < 940) {
+
+    if (screenWidth )
+
+    if (screenWidth > 900 && screenWidth <= 1090) {
+        navCarouselPortfolio.classList.add('display-none')             
+        containerCarouselPortfolio.style.paddingTop = '50px'
+
+        containerProject.forEach((containerProject) => {
+            containerProject.style.padding = '0px 20px'
+        }) 
+
+        //----Undo < 940px
+        dotsCarouselPortfolio.classList.remove('display-none')
+        navCarouselPortfolio.classList.add('owl-nav-portfolio-style')
+        navCarouselPortfolio.classList.remove('nav-style-screen940px')
+        nextButton.classList.add('owl-next-carouselPortfolio-style')
+        prevButton.classList.add('owl-prev-carouselPortfolio-style')
+        nextButton.classList.remove('buttonsNav-portfolio-screen940px')
+        prevButton.classList.remove('buttonsNav-portfolio-screen940px')
+
+        containerProject.forEach((containerProject) => {
+            containerProject.classList.remove('responsive-displayColumn-carouselPortfolio')
+        })
+
+        videoCarouselPortfolio.forEach((video) => {
+            video.style.width = '60%'
+            video.style.marginBottom = '0px'
+        })
+
+        infoProjectCollection.forEach((infoProject) => {
+            infoProject.classList.remove('infoProject-style-screen940px')
+        })
+
+        //-----------------------------------------------    
+    
+    }    
+
+    
+
+    if (screenWidth <= 900) {
+        
         dotsCarouselPortfolio.classList.add('display-none')
         navCarouselPortfolio.classList.remove('display-none', 'owl-nav-portfolio-style')
         navCarouselPortfolio.classList.add('nav-style-screen940px')
@@ -65,31 +109,5 @@ window.onresize = () => {
             infoProject.classList.add('infoProject-style-screen940px')
         })
 
-    }
-    if(screenWidth >= 940) {
-        dotsCarouselPortfolio.classList.remove('display-none')
-
-        if(screenWidth < 1320) {
-
-            navCarouselPortfolio.classList.add('display-none', 'owl-nav-portfolio-style')
-            navCarouselPortfolio.classList.remove('nav-style-screen940px')
-            nextButton.classList.add('owl-next-carouselPortfolio-style')
-            prevButton.classList.add('owl-prev-carouselPortfolio-style')
-            nextButton.classList.remove('buttonsNav-portfolio-screen940px')
-            prevButton.classList.remove('buttonsNav-portfolio-screen940px')
-            
-            containerProject.forEach((containerProject) => {
-                containerProject.classList.remove('responsive-displayColumn-carouselPortfolio')
-            }) 
-
-            videoCarouselPortfolio.forEach((video) => {
-                video.style.width = '570px'
-                video.style.marginBottom = '0px'
-            })
-
-            infoProjectCollection.forEach((infoProject) => {
-                infoProject.classList.remove('infoProject-style-screen940px')
-            })
-        }
     }
 };
